387be559a8e3ad6bce6c455e88e02b8a5a98cb19
ahmedabdelhamedsayed71 <email@github.com>
2026-07-01 11:27:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+require('dotenv').config()
+app.use(express.json());
+
+const port = process.env.PORT;
+const DB_URL = process.env.DB_URL;
+
+
+mongoose
+.connect(DB_URL)
+.then(()=>{
+  console.log("DB Connected")
+})
+.catch(()=>{
+  console.log("DB Not Connected")
+  process.exit(1)
+})
+
+app.get("/", (req, res)=>{
+  res.status(200).json ({
+    msg: "SkillSwap",
+  })
+})
+
+module.exports = "app";
