e569b1fe95e4bae3d00112a664d933b95682e0a1
OmarTarek1806 <tarekomar015@gmail.com>
2026-07-09 21:53:42 +0300

    Add authentication API with JWT cookies

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+<<<<<<< HEAD
 const express = require('express');
+=======
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const authRoutes = require("./routes/authRoutes");
+
+const skillRoutes = require("./routes/skillRoutes");
+const testRoutes = require("./routes/testRoutes");
+
+>>>>>>> 0b35c47 (Add authentication API with JWT cookies)
 const app = express();
 const mongoose = require('mongoose');
 const skillRoutes = require('./routes/skillRoutes');
@@ -6,9 +19,22 @@ const testRoutes = require('./routes/testRoutes');
 require('dotenv').config();
 
 app.use(express.json());
+<<<<<<< HEAD
 app.use('/api/skills', skillRoutes);
 app.use('/api/questions', testRoutes);
 app.use('/api/tests', testRoutes);
+=======
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
+
+app.use("/api/auth", authRoutes);
+>>>>>>> 0b35c47 (Add authentication API with JWT cookies)
 
 const port = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL;
