b822e83c29d6deb18ab3f4e6d40e1154ce231c4a
OmarTarek1806 <tarekomar015@gmail.com>
2026-07-14 04:16:19 +0300

    Fix backend index routes

--- index.js
+++ index.js
@@ -1,69 +1,63 @@
-const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/authRoutes');
-const skillRoutes = require('./routes/skillRoutes');
-const testRoutes = require('./routes/testRoutes');
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const requestRoutes = require("./routes/requestRoutes");
+const contactRoutes = require("./routes/contactRoutes");
+const skillRoutes = require("./routes/skillRoutes");
+const testRoutes = require("./routes/testRoutes");
+const resultRoutes = require("./routes/resultRoutes");
+
 const app = express();
 
-const userRoutes = require('./routes/userRoutes');
-const requestRoutes = require('./routes/requestRoutes');
-const contactRoutes = require('./routes/contactRoutes');
-const skillRoutes = require('./routes/skillRoutes');
-const testRoutes = require('./routes/testRoutes');
-
-app.use(express.json());
-app.use('/api/skills', skillRoutes);
-app.use('/api/questions', testRoutes);
-app.use('/api/tests', testRoutes);
-app.use(cookieParser());
-
+// Middlewares
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
-  }),
+  })
 );
 
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/requests', requestRoutes);
-app.use('/api/contact', contactRoutes);
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/api/skills', skillRoutes);
-app.use('/api/questions', testRoutes);
-app.use('/api/tests', testRoutes);
-app.use('/api/results', resultRoutes);
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/requests", requestRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/skills", skillRoutes);
+app.use("/api/questions", testRoutes);
+app.use("/api/tests", testRoutes);
+app.use("/api/results", resultRoutes);
 
-const port = process.env.PORT || 3000;
+// Home Route
+app.get("/", (req, res) => {
+  res.status(200).json({
+    msg: "SkillSwap",
+  });
+});
+
+// Database
+const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.DB_URL;
 
 async function connectDB() {
   try {
     await mongoose.connect(DB_URL);
-    console.log('DB Connected');
-    return mongoose;
+    console.log("DB Connected");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
   } catch (error) {
-    console.error('DB Not Connected', error);
+    console.error("DB Not Connected", error);
     process.exit(1);
   }
 }
 
 connectDB();
-
-app.get('/', (req, res) => {
-  res.status(200).json({
-    msg: 'SkillSwap',
-  });
-});
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
-app.get('/', (req, res) => {
-  res.status(200).json({
-    msg: 'SkillSwap',
-  });
-});
